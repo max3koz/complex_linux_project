4d8215021126d7f804a9aaf13dd09d3865ca7509
Maksym Trykoz <max3koz@gmail.com>
2026-05-29 18:47:35 +0200

    Init progect repo

---
+++ calc.h
@@ -0,0 +1,11 @@
+#ifndef CALC_H
+#define CALC_H
+
+double add(double a, double b);
+double diff(double a, double b);
+double multiply(double a, double b);
+double divide(double a, double b);
+unsigned int factorial(unsigned int n);
+double square_root(double a);
+
+#endif
